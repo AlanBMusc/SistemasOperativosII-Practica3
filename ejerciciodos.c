7f5d2375867cbc74955b4c68ba731a2a4b9295ca
Adriana Sánchez-Bravo Cuesta <adriana.sanchez-bravo@rai.usc.es>
2026-04-17 00:33:54 +0200

    Update ejerciciodos.c

--- ejerciciodos.c
+++ ejerciciodos.c
@@ -5,35 +5,41 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <pthread.h>
-// #include <semaphore.h> // ELIMINADA: Se sustituye por mutex y variables de condición
 
 #define N 10
-#define TOTAL 80 // CAMBIADA: De 9 a 80 para cumplir el apartado 2
+#define TOTAL 80 
 
+//buffer compartido
 int buffer[N];
 int num_elementos = 0; 
 int principio = 0;
 int final = 0;
 
-// contadores de progreso
+//contadores de progreso
 int producidos = 0;
 int consumidos = 0;
 
-// contadores de suma
+//numero de hilos
+int num_p, num_c;
+
+//contadores de suma
 int suma_prod = 0;
 int suma_cons = 0;
 
-// Sincronización (Mutex y Variables de Condición)
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // NUEVA: Sustituye sem_buffer
-pthread_cond_t condp = PTHREAD_COND_INITIALIZER;    // NUEVA: Sustituye huecos
-pthread_cond_t condc = PTHREAD_COND_INITIALIZER;    // NUEVA: Sustituye elementos
+int fin= 0;
+
+//mutex 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+//variables de condicion
+pthread_cond_t condp = PTHREAD_COND_INITIALIZER; 
+pthread_cond_t condc = PTHREAD_COND_INITIALIZER; 
 
 FILE *f;
 
-// --- FUNCIONES OBLIGATORIAS (Apartado 1.3) ---
-
-int produce_item() { // NUEVA: Función requerida
+//cada llamada lee un número del archivo de texto y hace suma
+int produce_item(){
     int num;
+    //se comprueba si se llegó al final
     if (fscanf(f, "%d", &num) == 1) {
         suma_prod += num;
         return num;
@@ -41,21 +47,24 @@ int produce_item() { // NUEVA: Función requerida
     return -1; // Fin de fichero
 }
 
-void insert_item(int item) { // NUEVA: Función requerida
+//colocar entero leido
+void insert_item(int item) {
     buffer[final] = item;
     final = (final + 1) % N;
     num_elementos++;
 }
 
-int remove_item() { // NUEVA: Función requerida
+//retirar del buffer entero y reemplazarlo por 0
+int remove_item() {
     int item = buffer[principio];
-    buffer[principio] = 0; // Reemplazar por 0 como pide el enunciado
+    buffer[principio] = 0; 
     principio = (principio + 1) % N;
     num_elementos--;
     return item;
 }
 
-void consume_item(int item) { // NUEVA: Función requerida
+//calcula suma enteros
+void consume_item(int item) { 
     suma_cons += item;
 }
 
@@ -63,27 +72,52 @@ void consume_item(int item) { // NUEVA: Función requerida
 void* funcion_productor(void* arg) {
     int id = *(int*)arg; 
 
-    for (int i = 0; i < TOTAL; i++) { // CAMBIADA: while(1) por bucle de iteraciones
-        int num = produce_item(); // CAMBIADA: Se usa la función obligatoria
-        if (num == -1) break; // NUEVA: Por si el archivo es más corto que TOTAL
+    //cambiar bucle infinito por bucle hasta el maximo de iteraciones definido
+    for (int i = 0; i < TOTAL; i++) { 
 
-        pthread_mutex_lock(&mutex); // CAMBIADA: sem_wait(&sem_buffer) por mutex_lock
-        while (num_elementos == N) { // CAMBIADA: sem_wait(&huecos) por while + cond_wait
+        //se bloquea el buffer con un mutex
+        pthread_mutex_lock(&mutex); 
+
+        //cuando se llega al maximo de elementos en el buffer se desbloquea el mutex y
+        //se bloquea el hilo hasta que un consumidor quite algun elemento
+        while (num_elementos == N) {
             pthread_cond_wait(&condp, &mutex);
         }
 
-        insert_item(num); // CAMBIADA: Lógica interna por llamada a función
+        //se lee el numero y se guarda en la variable num
+        int num = produce_item(); 
+        //si se llego al final del archivo se sale del bucle
+        if (num == -1){
+            if (!fin){
+                fin= 1;
+
+                for (int i = 0; i < num_c; i++) {
+                    while (num_elementos == N)
+                        pthread_cond_wait(&condp, &mutex);
+
+                    insert_item(-1);
+                }
+                pthread_cond_broadcast(&condc);
+            }
+            pthread_mutex_unlock(&mutex);
+            break;
+        } 
+
+        //se añade al buffer el numero leido
+        insert_item(num); 
         producidos++; 
 
         printf("Productor %d mete '%d' (%d/%d)\n", id, num, producidos, TOTAL);
 
-        pthread_cond_signal(&condc); // CAMBIADA: sem_post(&elementos) por cond_signal
-        pthread_mutex_unlock(&mutex); // CAMBIADA: sem_post(&sem_buffer) por mutex_unlock
+        //si hay algun hilo consumidor bloqueado, lo desbloquea para que siga consumiendo
+        pthread_cond_broadcast(&condc);
+        //se desbloquea la memoria compartida despues de que el hilo acabe
+        pthread_mutex_unlock(&mutex); 
         
-        // --- CONTROL DE VELOCIDAD (Apartado 2.1.3) ---
-        if (i < 30) usleep(20000);  // NUEVA: Productor rápido (0.02s)
-        else if (i < 60) usleep(200000); // NUEVA: Productor lento (0.2s)
-        else usleep(100000); // NUEVA: Normal
+        // sleeps para las carreras criticas
+        if (i < 30) usleep(20000);  
+        else if (i < 60) usleep(200000); 
+        else usleep(100000);
     }
 
     pthread_exit(NULL);
@@ -93,33 +127,48 @@ void* funcion_productor(void* arg) {
 void* funcion_consumidor(void* arg) {
     int id = *(int*)arg;
 
-    for (int i = 0; i < TOTAL; i++) { // CAMBIADA: while(1) por bucle de iteraciones
-        pthread_mutex_lock(&mutex); // CAMBIADA: sem_wait(&sem_buffer) por mutex_lock
-        while (num_elementos == 0) { // CAMBIADA: sem_wait(&elementos) por while + cond_wait
+    for (int i = 0; i < TOTAL; i++) { 
+        //se bloquea el buffer ya que el consumidor entro
+        pthread_mutex_lock(&mutex); 
+        //si el buffer esta vacio, se espera a q algun productor lo llene
+        //con una variable de condicion
+        while (num_elementos == 0 ) {
+            if(fin){
+                pthread_mutex_unlock(&mutex);
+                pthread_exit(NULL);
+            } 
             pthread_cond_wait(&condc, &mutex);
         }
 
-        int num = remove_item(); // CAMBIADA: Lógica interna por llamada a función
-        consumidos++; 
+        //se retira el numero del buffer y se cambia por 0
+        int num = remove_item(); 
+        
+        if(num == -1){            
+            pthread_mutex_unlock(&mutex);
+            pthread_exit(NULL);
+        }
 
+        consumidos++; 
         printf("\tConsumidor %d saca '%d' (%d/%d)\n", id, num, consumidos, TOTAL);
 
-        pthread_cond_signal(&condp); // CAMBIADA: sem_post(&huecos) por cond_signal
-        pthread_mutex_unlock(&mutex); // CAMBIADA: sem_post(&sem_buffer) por mutex_unlock
+        //si algun productor estaba bloqueado se libera
+        pthread_cond_signal(&condp); 
+        //se desbloquea el acceso a memoria compartida 
+        pthread_mutex_unlock(&mutex); 
 
-        consume_item(num); // CAMBIADA: Lógica interna por llamada a función
+        //se hace la suma de los consumidores
+        consume_item(num); 
         
-        // --- CONTROL DE VELOCIDAD (Apartado 2.1.3 - Inverso) ---
-        if (i < 30) usleep(200000); // NUEVA: Consumidor lento para que el buffer se llene
-        else if (i < 60) usleep(20000); // NUEVA: Consumidor rápido para que se vacíe
-        else usleep(100000); // NUEVA: Normal
+        // velocidades segun el hilo 
+        if (i < 30) usleep(200000);
+        else if (i < 60) usleep(20000); 
+        else usleep(100000); 
     }
 
     pthread_exit(NULL);
 }
 
 int main() {
-    int num_p, num_c;
 
     printf("Numero de productores: ");
     scanf("%d", &num_p);
@@ -137,10 +186,7 @@ int main() {
         return 1;
     }
 
-    // ELIMINADAS: sem_init (huecos, elementos, sem_buffer) ya no se usan
-    // Los mutex y cond_var ya están inicializados de forma estática arriba
-
-    // creamos los hilos
+    //se crean los hilos
     pthread_t hilos_p[num_p];
     pthread_t hilos_c[num_c];
     int id_p[num_p];
@@ -156,7 +202,7 @@ int main() {
         pthread_create(&hilos_c[i], NULL, funcion_consumidor, &id_c[i]);
     }
 
-    // esperamos a que todos terminen
+    //se espera a que todos terminen
     for (int i = 0; i < num_p; i++) pthread_join(hilos_p[i], NULL);
     for (int i = 0; i < num_c; i++) pthread_join(hilos_c[i], NULL);
 
@@ -165,10 +211,9 @@ int main() {
     printf("Total consumidos: %d\n", consumidos);
     printf("Suma del consumidor= %d\n", suma_cons);
 
-    // ELIMINADAS: sem_destroy (huecos, elementos, sem_buffer)
-    pthread_mutex_destroy(&mutex); // NUEVA
-    pthread_cond_destroy(&condp);   // NUEVA
-    pthread_cond_destroy(&condc);   // NUEVA
+    pthread_mutex_destroy(&mutex); 
+    pthread_cond_destroy(&condp);   
+    pthread_cond_destroy(&condc);   
     fclose(f);
 
     return 0;
